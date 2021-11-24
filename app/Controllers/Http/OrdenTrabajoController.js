5b4597150c6541040e39aab3e7b9e06a0a16c4fb
Emmely Intriago <gabyintriago07@gmail.com>
2021-11-24 13:01:24 -0500

    rrr

--- app/Controllers/Http/OrdenTrabajoController.js
+++ app/Controllers/Http/OrdenTrabajoController.js
@@ -74,7 +74,7 @@ class OrdenTrabajoController {
         try {
             
             const{ordenTrabajoId} = request.params;
-            const ordenTrabajo = await Database.raw("select codigo, fecharegistro,  proveedor, lote, estadoCalidad from ordenTrabajo where id= '"+ordenTrabajoId+"' " )
+            const ordenTrabajo = await Database.raw("select codigo, fecharegistro,  proveedor, lote, estadoCalidad from ordenTrabajo where id= '"+ordenTrabajoId+"' ;" )
 
             return response.status(200).send({ordenTrabajo:ordenTrabajo[0]})
 
